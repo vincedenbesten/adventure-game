f632d495ecfc5efd8aefb5f016315a15af5b51c6
vince <99043881@mydavinci.nl>
2017-11-27 12:00:20 +0100

    progres
    finished the swimoption

--- adventure-game/javascript.js
+++ adventure-game/javascript.js
@@ -28,9 +28,30 @@ function beginspel() {
   document.getElementById("zwemmen").onclick = zwem;
 }
 
+function again() {
+  document.getElementById("levels").style.display = "none";
+  document.getElementById("levelnaam").innerHTML = "Overzicht";
+  document.getElementById("introbruggen").style.display = "block";
+  document.getElementById("zwemmen").style.display = "block";
+  document.getElementById("overzicht").style.backgroundImage = "url(css/grfx/begingame.jpg)";
+}
+
 function zwem() {
   document.getElementById("levelnaam").innerHTML = "Zwemmen";
   document.getElementById("introbruggen").style.display = "none";
   document.getElementById("zwemmen").style.display = "none";
   document.getElementById("levels").style.display = "flex";
+  document.getElementById("leveltekst").innerHTML = "Je bent bij het water aangekomen.<br>Zo moeilijk is het niet om even naar de overkant te zwemmen...<br>";
+  document.getElementById("option1").style.display = "block";
+  document.getElementById("option1").innerHTML = "Zwem naar de overkant";
+  document.getElementById("option1").onclick = drown;
+  document.getElementById("overzicht").style.backgroundImage = "url(css/grfx/zwemmen.jpg)";
+}
+
+function drown() {
+  document.getElementById("levelnaam").innerHTML = "GAMEOVER";
+  document.getElementById("leveltekst").innerHTML = "De stroming trok je onder, hierdoor verdronk je.";
+  document.getElementById("option1").innerHTML = "try again";
+  document.getElementById("option1").onclick = again;
+  document.getElementById("overzicht").style.backgroundImage = "url(css/grfx/drown.jpg)";
 }
